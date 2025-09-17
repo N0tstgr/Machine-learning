745f5f9eabd7ed9383cffc507c929bf0865af7cc
Your Name <vikashkasaudhan321@gmail.com>
2025-09-17 19:01:35 +0530

    settingpycharm

---
+++ Stranger/__init__.py
@@ -0,0 +1,10 @@
+import sys
+import math
+
+print("✅ Python is working in PyCharm!")
+print("Python version:", sys.version)
+print("Executable path:", sys.executable)
+
+# Small math test
+num = 16
+print(f"The square root of {num} is:", math.sqrt(num))
